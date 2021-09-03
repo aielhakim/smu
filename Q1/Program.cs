f3625227ef562abb68d2af910b679ccd69d3a55b
Ashraf Elhakim <aelhakim@remsoft.com>
2021-09-03 18:49:28 -0300

    Q1

--- Q1/Program.cs
+++ Q1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Q1
 {
@@ -6,7 +7,41 @@ namespace Q1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = readA();
+
+            for (int i = 1; i <= a; i++)
+            {
+                Console.WriteLine(Calculate(a, i));
+            }
+
+            Console.Read();
         }
+
+        private static int readA()
+        {
+            Console.Write("Enter the value for a: ");
+
+            string aText = Console.ReadLine();
+            int a;
+
+            if (int.TryParse(aText, out a))
+            {
+                return a;
+            }
+            else
+            {
+                return readA();
+            }
+        }
+
+        private static decimal Calculate(int a, int x)
+        {
+            var eqPart1 = decimal.Parse(Math.Pow(a, (2 * x)).ToString(), NumberStyles.Float);
+            var eqPart2 = decimal.Parse(Math.Pow(a, x).ToString(), NumberStyles.Float);
+            var eqPart3 = decimal.Parse("1", NumberStyles.Float);
+
+            return Math.Round(eqPart1 - eqPart2  + eqPart3);
+        }
+
     }
 }
