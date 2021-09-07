88b5cf4d6d4b2bbbfb32cb99b4a1d8b010d7f60f
Ashraf Elhakim <aelhakim@remsoft.com>
2021-09-07 10:58:44 -0300

    Q2

---
+++ Q2/MortgageEntity.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+    class MortgageEntity
+    {
+        public string borrowerName { get; set; }
+        public double mortgageBalance { get; set; }
+        public double annualInterestRate { get; set; }
+        public double currentMonthlyPayment { get; set; }
+        public double extraMonthlyPayment { get; set; }
+        public double newMonthlyPayment { get { return calculateNewPayment(currentMonthlyPayment, extraMonthlyPayment); } }
+        public int currentDurationInMonths { get { return calculateDurationInMonths(currentMonthlyPayment, annualInterestRate, mortgageBalance); } }
+        public int newDurationInMonths { get { return calculateDurationInMonths(newMonthlyPayment, annualInterestRate, mortgageBalance); } }
+        public double currentInterestAmount { get { return calculateInterestAmount(currentMonthlyPayment, currentDurationInMonths, mortgageBalance); } }
+        public double newInterestAmount { get { return calculateInterestAmount(newMonthlyPayment, newDurationInMonths, mortgageBalance); } }
+        public double newPaymentSavings { get { return currentInterestAmount - newInterestAmount;  } }
+        public bool extraFees { get { return newDurationInMonths <= currentDurationInMonths / 2; } }
+
+        public string extraFeesText { get { return extraFees ? " Extra Fees" : " No Fee"; } }
+        public string mortgageBalanceText { get { return string.Format(" ${0:0.00}", Math.Round(mortgageBalance, 2)); } }
+        public string annualInterestRateText { get { return string.Format(" {0:0.000}%", Math.Round(annualInterestRate, 3)); } }
+        public string currentMonthlyPaymentText { get { return string.Format(" ${0:0.00}", Math.Round(currentMonthlyPayment, 2)); } }
+        public string currentInterestAmountText { get { return string.Format(" ${0:0.00}", Math.Round(currentInterestAmount, 2)); } }
+        public string newMonthlyPaymentText { get { return string.Format(" ${0:0.00}", Math.Round(newMonthlyPayment, 2)); } }
+        public string newInterestAmountText { get { return string.Format(" ${0:0.00}", Math.Round(newInterestAmount, 2)); } }
+        public string newPaymentSavingsText { get { return string.Format(" ${0:0.00}", Math.Round(newPaymentSavings, 2)); } }
+
+        public string currentDurationInYearsMonths
+        {
+            get { return calculateDurationInYearsMonths(currentDurationInMonths); }
+        }
+        public string newDurationInYearsMonths
+        {
+            get { return calculateDurationInYearsMonths(newDurationInMonths); }
+        }
+
+        private static string calculateDurationInYearsMonths(int durationInMonths)
+        {
+            int durationYears = durationInMonths / 12;
+            int durationYearMonthRestOnly = durationInMonths % 12;
+
+            return string.Format("{0}yrs {1}mo", durationYears, durationYearMonthRestOnly);
+        }
+
+        private static double calculateNewPayment(double currentMonthlyPayment, double extraMonthlyPayment)
+        {
+            return currentMonthlyPayment + extraMonthlyPayment;
+        }
+
+        private static int calculateDurationInMonths(double monthlyPayment, double interestRate, double mortagageBalance)
+        {
+            double i = interestRate / 100 / 12;
+
+            return (int)Math.Round(
+                Math.Log((monthlyPayment / i) / ((monthlyPayment / i) - mortagageBalance))
+                / Math.Log(1 + i));
+        }
+
+        private static double calculateInterestAmount(double monthlyPayment, double durationInMonths, double mortagageBalance)
+        {
+            return (monthlyPayment * durationInMonths) - mortagageBalance;
+        }
+    }
+}
---
+++ Q2/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Q2
+{
+    class Program
+    {
+       static List<MortgageEntity> mortgageEntities;
+
+        static void Main(string[] args)
+        {
+            mortgageEntities = new List<MortgageEntity>();
+            int n = readN();
+
+            for (int i = 0; i < n; i++)
+            {
+                int borrowerNumber = i + 1;
+                mortgageEntities.Add(new MortgageEntity
+                {
+                    borrowerName = readBorrowerName(borrowerNumber),
+                    mortgageBalance = readMortgageBalance(borrowerNumber),
+                    annualInterestRate = readAnnualInterestRate(borrowerNumber),
+                    currentMonthlyPayment = readCurrentMonthlyPayment(borrowerNumber),
+                    extraMonthlyPayment = readExtraMonthlyPayment(borrowerNumber)
+                });
+                Console.WriteLine("");
+                Console.WriteLine("");
+            }
+            writeResults();
+            Console.Read();
+        }
+
+        private static int readN()
+        {
+            Console.Write("Enter the number of entries to calculate mortgage: ");
+
+            string nText = Console.ReadLine();
+            int n;
+
+            if (int.TryParse(nText, out n))
+            {
+                return n;
+            }
+            else
+            {
+                return readN();
+            }
+        }
+
+        private static string readBorrowerName(int number)
+        {
+            Console.Write(number + "- Enter the name of the borrower: ");
+
+            return Console.ReadLine();
+        }
+
+        private static double readMortgageBalance(int number)
+        {
+            Console.Write(number + "- Enter the mortgage balance: $");
+
+            string mbText = Console.ReadLine();
+            double mb;
+
+            if (double.TryParse(mbText, out mb))
+            {
+                return mb;
+            }
+            else
+            {
+                return readMortgageBalance(number);
+            }
+        }
+
+        private static double readAnnualInterestRate(int number)
+        {
+            Console.Write(number + "- Enter the annual interest rate: ");
+
+            string aiText = Console.ReadLine();
+            double ai;
+
+            if (double.TryParse(aiText, out ai))
+            {
+                return ai;
+            }
+            else
+            {
+                return readAnnualInterestRate(number);
+            }
+        }
+
+        private static double readCurrentMonthlyPayment(int number)
+        {
+            Console.Write(number + "- Enter the current monthly payment: $");
+
+            string mpText = Console.ReadLine();
+            double mp;
+
+            if (double.TryParse(mpText, out mp))
+            {
+                return mp;
+            }
+            else
+            {
+                return readCurrentMonthlyPayment(number);
+            }
+        }
+
+        private static double readExtraMonthlyPayment(int number)
+        {
+            Console.Write(number + "- Enter the extra monthly payment: $");
+
+            string empText = Console.ReadLine();
+            double emp;
+
+            if (double.TryParse(empText, out emp))
+            {
+                return emp;
+            }
+            else
+            {
+                return readExtraMonthlyPayment(number);
+            }
+        }
+
+        public static void writeResults()
+        {
+            string rowStrurcture = "{0,-20} {1,-20} {2,-20} {3,-10} {4,-10} {5,-15} {6,-10} {7,-10} {8,-15} {9,-10} {10,-10}";
+
+            Console.WriteLine(string.Format(rowStrurcture,
+                "Name", "|Mortgage Balance", "|Interest Rate", "|", "Current",
+                "", "|", "New", "", " |Savings", " |Fees"));
+
+            Console.WriteLine(string.Format(rowStrurcture,
+                "", "", "", " Payment", " Duration",
+                " Interset", "| Payment", "Duration", " Interest", "", ""));
+
+
+            foreach (var item in mortgageEntities)
+            {
+                Console.WriteLine(string.Format(rowStrurcture,
+               item.borrowerName, item.mortgageBalanceText, item.annualInterestRateText,
+               item.currentMonthlyPaymentText, item.currentDurationInYearsMonths, item.currentInterestAmountText,
+               item.newMonthlyPaymentText, item.newDurationInYearsMonths, item.newInterestAmountText,
+               item.newPaymentSavingsText, item.extraFeesText));
+            }
+        }
+    }
+}
